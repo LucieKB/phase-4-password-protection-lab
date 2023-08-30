6172e0396346f4feb90252e53f08e3ca09c0b364
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2023-08-30 14:10:14 -0600

    commit tree 4a4021b52e6a8798c9b920d9c1ba35f99f7f7503

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+skip_before_action :authorized, only: :create
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created 
+    end
+
+    def show
+        current_user = User.find(session[:user_id])
+        render json: current_user
+    end
+
+    private
+
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+
+    def render_unprocessable_entity(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
+end
